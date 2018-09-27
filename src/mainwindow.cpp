9e404b14f0f4cb7896032d9b5653bc61e4bc08a5
zanyxdev <zanyxdev@gmail.com>
2018-09-27 10:31:23 +0300

    add empty slots and create alfa version

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,27 @@
-#include <QtWidgets>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QStatusBar>
+#include <QTextEdit>
 #include <QByteArray>
+#include <QMenu>
+#include <QMenuBar>
+#include <QToolBar>
+#include <QApplication>
+#include <QDesktopWidget>
+
+#if defined(QT_PRINTSUPPORT_LIB)
+#include <QtPrintSupport/qtprintsupportglobal.h>
+#if QT_CONFIG(printer)
+#if QT_CONFIG(printdialog)
+#include <QPrintDialog>
+#endif
+
+#include <QPrinter>
+#if QT_CONFIG(printpreviewdialog)
+#include <QPrintPreviewDialog>
+#endif
+#endif
+#endif
 
 #include "mainwindow.h"
 
@@ -7,6 +29,7 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     setupFileMenu();
+    setupPrintMenu();
     setupHelpMenu();
     setupEditor();
 
@@ -15,23 +38,35 @@ MainWindow::MainWindow(QWidget *parent)
     setCentralWidget(editor);
     setWindowTitle(tr("CHIP-8 Assembler syntax highlighter"));
 
+    QDesktopWidget desktop;
+    desktopRect = desktop.availableGeometry(desktop.primaryScreen());
+
+    this->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::MinimumExpanding);
+    this->setMaximumSize(desktopRect.width() - 5, desktopRect.height() - 5);
+
+    //Move app window to center desktop
+    this->move(calcDeskTopCenter(this->width(),this->height()));
+    this->setMinimumSize(800,600);
+
 }
+
 void MainWindow::about()
 {
     QMessageBox::about(this, tr("About Syntax Highlighter"),
-                tr("<p>The <b>Syntax Highlighter</b> example shows how " \
-                   "to perform simple syntax highlighting by subclassing " \
-                   "the QSyntaxHighlighter class and describing " \
-                   "highlighting rules using regular expressions.</p>"));
+                       tr("<p>The <b>Syntax Highlighter</b> example shows how " \
+                          "to perform simple syntax highlighting by subclassing " \
+                          "the QSyntaxHighlighter class and describing " \
+                          "highlighting rules using regular expressions.</p>"));
 }
-void MainWindow::openFile(const QString &path)
-{
-    QString fileName = path;
 
-    if (fileName.isNull())
-        fileName = QFileDialog::getOpenFileName(this,
-                                                tr("Open File"),
-                                                "", "CHIP-8 Files (*.ch8 *.dat)");
+void MainWindow::fileOpen()
+{
+    QString selectedFilter;
+    QString fileName = QFileDialog::getOpenFileName(this,
+                                                    tr("Select chip-8 game file"),
+                                                    "",
+                                                    tr("Chip-8 game files (*.ch8)"),
+                                                    &selectedFilter);
 
     if (!fileName.isEmpty()) {
 
@@ -41,10 +76,59 @@ void MainWindow::openFile(const QString &path)
         }
         QByteArray tmp = file.readAll();
         emit fileLoaded( tmp );
-
-
-       // editor->setPlainText(file.readAll());
     }
+}
+
+bool MainWindow::fileSave()
+{
+    return true;
+}
+
+void MainWindow::filePrint()
+{
+
+}
+
+void MainWindow::filePrintPreview()
+{
+#if QT_CONFIG(printpreviewdialog)
+    QPrinter printer(QPrinter::HighResolution);
+    QPrintPreviewDialog preview(&printer, this);
+    connect(&preview, &QPrintPreviewDialog::paintRequested, this, &MainWindow::printPreview);
+    preview.exec();
+#endif
+}
+
+void MainWindow::printPreview(QPrinter *printer)
+{
+#ifdef QT_NO_PRINTER
+    Q_UNUSED(printer);
+#else
+    editor->print(printer);
+#endif
+}
+
+void MainWindow::filePrintPdf()
+{
+#ifndef QT_NO_PRINTER
+
+    QFileDialog fileDialog(this, tr("Export PDF"));
+    fileDialog.setAcceptMode(QFileDialog::AcceptSave);
+    fileDialog.setMimeTypeFilters(QStringList("application/pdf"));
+    fileDialog.setDefaultSuffix("pdf");
+    if (fileDialog.exec() != QDialog::Accepted){
+
+        return;
+    }
+    QString fileName = fileDialog.selectedFiles().first();
+    QPrinter printer(QPrinter::HighResolution);
+    printer.setOutputFormat(QPrinter::PdfFormat);
+    printer.setOutputFileName(fileName);
+    editor->document()->print(&printer);
+    statusBar()->showMessage(tr("Exported \"%1\"")
+                             .arg(QDir::toNativeSeparators(fileName)));
+
+#endif
 }
 
 void MainWindow::setupEditor()
@@ -58,8 +142,6 @@ void MainWindow::setupEditor()
     editor->setFont(font);
 
     //highlighter = new Highlighter(editor->document());
-
-
 }
 
 void MainWindow::setupFileMenu()
@@ -67,8 +149,20 @@ void MainWindow::setupFileMenu()
     QMenu *fileMenu = new QMenu(tr("&File"), this);
     menuBar()->addMenu(fileMenu);
 
-    fileMenu->addAction(tr("&Open..."), this, SLOT(openFile()), QKeySequence::Open);
+    fileMenu->addAction(tr("&Open..."), this, SLOT(fileOpen()), QKeySequence::Open);
     fileMenu->addAction(tr("E&xit"), qApp, SLOT(quit()), QKeySequence::Quit);
+}
+
+void MainWindow::setupPrintMenu()
+{
+#ifndef QT_NO_PRINTER
+    QMenu *printMenu = new QMenu(tr("&Print"), this);
+    menuBar()->addMenu(printMenu);
+
+    printMenu->addAction(tr("&Print..."), this, SLOT(filePrint()), QKeySequence::Print);
+    printMenu->addAction(tr("&Export PDF..."), this, SLOT(filePrintPdf()), Qt::CTRL + Qt::Key_D);
+
+#endif
 }
 
 void MainWindow::setupHelpMenu()
@@ -84,3 +178,15 @@ MainWindow::~MainWindow()
 {
 
 }
+
+QPoint MainWindow::calcDeskTopCenter(int width,int height)
+{
+
+    QPoint centerWindow;
+    //получаем прямоугольник с размерами как у экрана
+    centerWindow = desktopRect.center(); //получаем координаты центра экрана
+    centerWindow.setX(centerWindow.x() - (width / 2) );
+    centerWindow.setY(centerWindow.y() - (height / 2) );
+    return centerWindow;
+}
+
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -4,7 +4,11 @@
 #include <QMainWindow>
 #include "decoder.h"
 QT_BEGIN_NAMESPACE
+class QPoint;
 class QTextEdit;
+class QByteArray;
+class QMenu;
+class QPrinter;
 class QByteArray;
 QT_END_NAMESPACE
 
@@ -17,17 +21,29 @@ public:
     ~MainWindow();
 signals:
     void fileLoaded(QByteArray &data);
+    void startEmulation();
+    void stopEmulation();
 public slots:
     void about();
-    void openFile(const QString &path = QString());
+
+private slots:
+    void fileOpen();
+    bool fileSave();
+    void filePrint();
+    void filePrintPreview();
+    void filePrintPdf();
+    void printPreview(QPrinter *);
 private:
     void setupEditor();
     void setupFileMenu();
+    void setupPrintMenu();
     void setupHelpMenu();
+    QPoint calcDeskTopCenter(int width,int height);
 
     QTextEdit *editor;
     Decoder *ch8Decoder;
-   // Highlighter *highlighter;
+    QRect desktopRect;
+    // Highlighter *highlighter;
 };
 
 #endif // MAINWINDOW_H
