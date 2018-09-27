acd49e2c9db19da7756d6b0aa19e36608786bc3c
zanyxdev <zanyxdev@gmail.com>
2018-09-27 05:40:42 +0300

    add fileLoaded signal

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,8 +1,83 @@
+#include <QtWidgets>
+#include <QByteArray>
+
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    setupFileMenu();
+    setupHelpMenu();
+    setupEditor();
+
+    ch8Decoder = new Decoder(this);
+    QObject::connect(this,&MainWindow::fileLoaded,ch8Decoder,&Decoder::toAsm);
+    setCentralWidget(editor);
+    setWindowTitle(tr("CHIP-8 Assembler syntax highlighter"));
+
+}
+void MainWindow::about()
+{
+    QMessageBox::about(this, tr("About Syntax Highlighter"),
+                tr("<p>The <b>Syntax Highlighter</b> example shows how " \
+                   "to perform simple syntax highlighting by subclassing " \
+                   "the QSyntaxHighlighter class and describing " \
+                   "highlighting rules using regular expressions.</p>"));
+}
+void MainWindow::openFile(const QString &path)
+{
+    QString fileName = path;
+
+    if (fileName.isNull())
+        fileName = QFileDialog::getOpenFileName(this,
+                                                tr("Open File"),
+                                                "", "CHIP-8 Files (*.ch8 *.dat)");
+
+    if (!fileName.isEmpty()) {
+
+        QFile file(fileName);
+        if (!file.open(QIODevice::ReadOnly)) {
+            return;
+        }
+        QByteArray tmp = file.readAll();
+        emit fileLoaded( tmp );
+
+
+       // editor->setPlainText(file.readAll());
+    }
+}
+
+void MainWindow::setupEditor()
+{
+    QFont font;
+    font.setFamily("Courier");
+    font.setFixedPitch(true);
+    font.setPointSize(10);
+
+    editor = new QTextEdit;
+    editor->setFont(font);
+
+    //highlighter = new Highlighter(editor->document());
+
+
+}
+
+void MainWindow::setupFileMenu()
+{
+    QMenu *fileMenu = new QMenu(tr("&File"), this);
+    menuBar()->addMenu(fileMenu);
+
+    fileMenu->addAction(tr("&Open..."), this, SLOT(openFile()), QKeySequence::Open);
+    fileMenu->addAction(tr("E&xit"), qApp, SLOT(quit()), QKeySequence::Quit);
+}
+
+void MainWindow::setupHelpMenu()
+{
+    QMenu *helpMenu = new QMenu(tr("&Help"), this);
+    menuBar()->addMenu(helpMenu);
+
+    helpMenu->addAction(tr("&About"), this, SLOT(about()));
+    helpMenu->addAction(tr("About &Qt"), qApp, SLOT(aboutQt()));
 }
 
 MainWindow::~MainWindow()
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,14 +2,32 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "decoder.h"
+QT_BEGIN_NAMESPACE
+class QTextEdit;
+class QByteArray;
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = 0);
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+signals:
+    void fileLoaded(QByteArray &data);
+public slots:
+    void about();
+    void openFile(const QString &path = QString());
+private:
+    void setupEditor();
+    void setupFileMenu();
+    void setupHelpMenu();
+
+    QTextEdit *editor;
+    Decoder *ch8Decoder;
+   // Highlighter *highlighter;
 };
 
 #endif // MAINWINDOW_H
