691191f15e80b5afcab6cfb2160225623a95fa67
zanyxdev <zanyxdev@gmail.com>
2018-10-08 11:31:40 +0300

    add ability highlighter for keywords

--- src/highlighter.cpp
+++ src/highlighter.cpp
@@ -1,6 +1,63 @@
 #include "highlighter.h"
 
-Highlighter::Highlighter()
-{
 
+Highlighter::Highlighter(QTextDocument *parent)
+    : QSyntaxHighlighter(parent)
+{
+    HighlightingRule rule;
+
+    keywordFormat.setForeground(Qt::darkBlue);
+    keywordFormat.setFontWeight(QFont::Bold);
+    QStringList keywordPatterns;
+    keywordPatterns << "\\bCLS\\b" << "\\bRET\\b" << "\\bSCD\\b"
+                    << "\\bSCR\\b" << "\\bSCL\\b" << "\\b \\b"
+                    << "\\bEXIT\\b"<< "\\bLOW\\b" << "\\bHIGH\\b"
+                    << "\\bJP\\b"  << "\\bCALL\\b"<< "\\bSE\\b"
+                    << "\\bSNE\\b" << "\\bLD\\b"  << "\\bADD\\b"
+                    << "\\bOR\\b"  << "\\bXOR\\b" << "\\bSUB\\b"
+                    << "\\bSHR\\b" << "\\bSUBN\\b"<< "\\bSHL\\b"
+                    << "\\bSNE\\b" << "\\bRND\\b" << "\\bDRW\\b"
+                    << "\\bSKP\\b" << "\\bSKNP\\b" ;
+
+    foreach (const QString &pattern, keywordPatterns) {
+        rule.pattern = QRegularExpression(pattern);
+        rule.format = keywordFormat;
+        highlightingRules.append(rule);
+
+        registerFormat.setFontWeight(QFont::Light);
+        registerFormat.setForeground(Qt::darkMagenta);
+        rule.pattern = QRegularExpression(" (DT|ST|(\\[I\\])|F|B|HF|R|V)");
+        rule.format = registerFormat;
+        highlightingRules.append(rule);
+
+        singleLineCommentFormat.setForeground(Qt::darkGreen);
+        rule.pattern = QRegularExpression(";[^\n]*");
+        rule.format = singleLineCommentFormat;
+        highlightingRules.append(rule);
+
+        //multiLineCommentFormat.setForeground(Qt::red);
+        //quotationFormat.setForeground(Qt::darkGreen);
+        //rule.pattern = QRegularExpression("\".*\"");
+        //rule.format = quotationFormat;
+        //highlightingRules.append(rule);
+
+        //functionFormat.setFontItalic(true);
+        //functionFormat.setForeground(Qt::blue);
+        //rule.pattern = QRegularExpression("\\b[A-Za-z0-9_]+(?=\\()");
+        //rule.format = functionFormat;
+        //highlightingRules.append(rule);
+    }
 }
+
+void Highlighter::highlightBlock(const QString &text)
+{
+    foreach (const HighlightingRule &rule, highlightingRules) {
+        QRegularExpressionMatchIterator matchIterator = rule.pattern.globalMatch(text);
+        while (matchIterator.hasNext()) {
+            QRegularExpressionMatch match = matchIterator.next();
+            setFormat(match.capturedStart(), match.capturedLength(), rule.format);
+        }
+    }
+}
+
+
--- src/highlighter.h
+++ src/highlighter.h
@@ -1,11 +1,36 @@
 #ifndef HIGHLIGHTER_H
 #define HIGHLIGHTER_H
 
+#include <QSyntaxHighlighter>
+#include <QTextCharFormat>
+#include <QRegularExpression>
 
-class Highlighter
+QT_BEGIN_NAMESPACE
+class QTextDocument;
+QT_END_NAMESPACE
+
+class Highlighter: public QSyntaxHighlighter
 {
+    Q_OBJECT
+
 public:
-    Highlighter();
+    Highlighter(QTextDocument *parent = 0);
+
+protected:
+    void highlightBlock(const QString &text);
+
+private:
+
+    struct HighlightingRule
+    {
+        QRegularExpression pattern;
+        QTextCharFormat format;
+    };
+
+    QVector<HighlightingRule> highlightingRules;
+    QTextCharFormat keywordFormat;
+    QTextCharFormat singleLineCommentFormat;
+    QTextCharFormat registerFormat;
 };
 
 #endif // HIGHLIGHTER_H
